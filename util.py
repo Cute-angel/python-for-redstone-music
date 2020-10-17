69f8728a6ff3c5d5be2298f3d4f338f6de1fc64a
Cute-angel <cute_xiaoguoguo@outlook.com>
2020-10-17 23:09:22 +0800

    add 2 fx in util

--- util.py
+++ util.py
@@ -1,2 +1,15 @@
 def single2many(start,end):
-    
+    firstp = start
+    secondp = end       #this is a list
+    finlist = []
+    for point in secondp:
+        finlist.append((firstp,point))
+    return finlist
+
+def many2many(start,end):
+    firstp,secondp = start,end          #all is list
+    finlist = []
+    for p1 in firstp:
+        for p2 in secondp:
+            finlist.append((p1,p2))
+    return finlist
